2b00572bf78ef3ead81c00f5a3b12dc0f6599182
Akash Biswal <akashbiswaliit7@gmail.com>
2024-07-27 23:23:48 +0530

    chat workspace done

--- app/src/main/java/com/codexnovas/e_ndore/chat_sent_by_employee.java
+++ app/src/main/java/com/codexnovas/e_ndore/chat_sent_by_employee.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class chat_sent extends AppCompatActivity {
+public class chat_sent_by_employee extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_chat_received);
+        setContentView(R.layout.activity_chat_sent_by_employee);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
